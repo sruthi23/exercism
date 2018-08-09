9bc2cbdab16789de2267f79764cc4b8e820c4916
sibizulu <sibiraj@lightrains.com>
2018-08-09 17:52:07 +0530

    solved 12 test cases

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,14 +1,11 @@
 defmodule Bob do
   def hey(input) do
     cond do
-    String.ends_with?(input,[".","!"]) and String.upcase(input) != input or
-     (String.contains?(input,"?") and String.contains?(input,"."))->
+    String.ends_with?(input,[".","!"]) and String.upcase(input) != input ->
      "Whatever."
-     String.upcase(input) == input and String.ends_with?(input,"!")->
-     "Whoa, chill out!"
-     String.ends_with?(input, "?") and String.upcase(input) != input-> "Sure."
-String.upcase(input) == input and String.ends_with?(input,"?")-> "Calm down, I know what I'm doing!"
-true -> "Fine. Be that way!"
+     String.trim(input) == "" -> "Fine. Be that way!"
+     String.ends_with?(input,"!") or String.upcase(input) == input -> "Whoa, chill out!"
+     String.match?(input, ~r/\d\?/)->"hhhh"
     end
   end
 end
