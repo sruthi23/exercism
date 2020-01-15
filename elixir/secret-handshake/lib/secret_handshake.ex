30f17449aaf6dd7d0ef4393d81137a78d824426d
sruthi23 <sruthi2525@gmail.com>
2020-01-15 10:38:36 +0530

    Completed

--- elixir/secret-handshake/lib/secret_handshake.ex
+++ elixir/secret-handshake/lib/secret_handshake.ex
@@ -1,19 +1,20 @@
 defmodule SecretHandshake do
-  @doc """
-  Determine the actions of a secret handshake based on the binary
-  representation of the given `code`.
+use Bitwise, only_operators: true
 
-  If the following bits are set, include the corresponding action in your list
-  of commands, in order from lowest to highest.
-
-  1 = wink
-  10 = double blink
-  100 = close your eyes
-  1000 = jump
-
-  10000 = Reverse the order of the operations in the secret handshake
-  """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+  map = %{1=>"wink", 2=>"double blink", 4=>"close your eyes", 8=>"jump"}
+
+  reverse? = (code &&& 16) != 0
+   Enum.reduce(map, [], fn {map, action}, acc ->
+     case code &&& map do
+       0 ->
+         acc
+       _ when reverse? ->
+         [action | acc]
+       _ ->
+         acc ++ [action]
+     end
+   end)
   end
 end
