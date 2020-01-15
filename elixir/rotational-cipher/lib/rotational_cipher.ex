05dc0a96be89366f03d57fe81d8af91ffbb8fe27
sruthi23 <sruthi2525@gmail.com>
2020-01-15 12:24:11 +0530

    Add function to shift letters

--- elixir/rotational-cipher/lib/rotational_cipher.ex
+++ elixir/rotational-cipher/lib/rotational_cipher.ex
@@ -1,4 +1,8 @@
 defmodule RotationalCipher do
+
+  @upper ?A..?Z
+  @lower ?a..?z
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,5 +12,16 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+       text
+       |> to_charlist
+       |> Enum.map(fn(x)->
+       cond do
+        x in @upper -> rem(x-?A+shift,26)+?A
+        x in @lower ->rem(x-?a+shift,26)+?a
+        true ->x
+        end
+         end)
+       |> to_string
+
   end
 end
