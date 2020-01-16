4a7988e6fd9a0083bb55bbb037cb7b7f13c96a95
sruthi23 <sruthi2525@gmail.com>
2020-01-16 12:32:40 +0530

    Add function to translate codons

--- elixir/protein-translation/lib/protein_translation.ex
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -1,9 +1,13 @@
 defmodule ProteinTranslation do
+
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: {atom, list(String.t())}
   def of_rna(rna) do
+      (for <<x::binary-3 <- rna>>, do: x )|>
+        Enum.each(fn x -> of_codon(x) end)
   end
 
   @doc """
@@ -29,5 +33,27 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: {atom, String.t()}
   def of_codon(codon) do
+  map = %{
+      "UGU" => "Cysteine",
+      "UGC" => "Cysteine",
+      "UUA" => "Leucine",
+      "UUG"=>"Leucine",
+      "AUG" => "Methionine",
+      "UUU"=>"Phenylalanine",
+      "UUC"=>"Phenylalanine",
+      "UCU"=>"Serine",
+      "UCC"=>"Serine",
+      "UCA"=>"Serine",
+      "UCG"=>"Serine",
+      "UGG"=>"Tryptophan",
+      "UAU"=>"Tyrosine",
+      "UAC"=>"Tyrosine",
+      "UAA"=>"STOP",
+      "UAG"=>"STOP",
+      "UGA"=>"STOP" }
+  case Map.fetch(map,codon) do
+    :error -> {:error, "invalid codon"} -> {:error, "invalid RNA"}
+    res -> res
+  end
   end
 end
